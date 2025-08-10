1d2acd3f71645a944d04e2582c0d3bbe58e48af8
candied apple <50497574+candied-apple@users.noreply.github.com>
2025-08-10 18:58:00 +0300

    reset

--- forge.config.js
+++ forge.config.js
@@ -35,12 +35,6 @@ module.exports = {
       }
     },
     {
-      name: '@electron-forge/maker-pkg',
-      config: {
-        identity: null // No signing
-      }
-    },
-    {
       name: '@electron-forge/maker-deb',
       config: {
         icon: './assets/icon.png'
